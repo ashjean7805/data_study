c579d29fd7569eb6cf22e6e05adaef4b2bbf7ca5
ashjean <ashjean.dev@gmail.com>
2023-04-17 23:06:50 +0900

    algorithms improve

--- Computer_Science/Algorithmns/Algorithmic Toolbox/Assignment_01_2_max_pairwise_product.py
+++ Computer_Science/Algorithmns/Algorithmic Toolbox/Assignment_01_2_max_pairwise_product.py
@@ -1,15 +1,9 @@
 def max_pairwise_product(numbers):
-    n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
-
-    return max_product
-
+    x = max(numbers)
+    numbers.remove(x)
+    return x * max(numbers)
 
 if __name__ == '__main__':
-    _ = int(input())
-    input_numbers = list(map(int, input().split()))
-    print(max_pairwise_product(input_numbers))
+    _ = input()
+    print(max_pairwise_product(list(map(int, input().split()))))
+
