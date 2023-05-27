c1cf33d52f215e53c42f249eaaa80f1e96938386
ashjean <ashjean.dev@gmail.com>
2023-05-27 16:41:14 +0900

    answer update

--- Computer_Science/Algorithmns/Algorithmic Toolbox/Chapter_5/4_lcs2.py
+++ Computer_Science/Algorithmns/Algorithmic Toolbox/Chapter_5/4_lcs2.py
@@ -1,5 +1,14 @@
 def lcs2(first_sequence, second_sequence):
-    return 0
+    n_first = len(first_sequence) 
+    n_second = len(second_sequence)
+    dp_matrix = [[0 for x in range(n_second+1)] for x in range(n_first+1)]
+    for i in range(1,n_first+1):
+        for j in range(1,n_second+1):
+            if first_sequence[i-1] == second_sequence[j-1]:
+                dp_matrix[i][j] = dp_matrix[i-1][j-1] + 1
+            else:
+                dp_matrix[i][j] = max(dp_matrix[i][j-1],dp_matrix[i-1][j])
+    return dp_matrix[n_first][n_second] 
 
 
 if __name__ == '__main__':
---
+++ Computer_Science/Algorithmns/Algorithmic Toolbox/Chapter_6/2_partition3.py
@@ -0,0 +1,41 @@
+# import sys
+from sys import stdin
+
+def partition3(loot):
+    total_sum = sum(loot)
+    target_sum = total_sum // 3
+    n = len(loot)
+
+    if n < 3 or total_sum % 3 != 0 or target_sum == 0:
+        return 0
+
+    count = 0
+
+    dp = [[0] * (n + 1) for _ in range(target_sum + 1)]
+
+    for i in range(1, target_sum + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i][j - 1]
+            if loot[j - 1] <= i:
+                temp = dp[i - loot[j - 1]][j - 1] + loot[j - 1]
+                if temp > dp[i][j]:
+                    dp[i][j] = temp
+            if dp[i][j] == target_sum:
+                count += 1
+
+    if count < 3:
+        return 0
+    else:
+        return 1
+
+
+if __name__ == '__main__':
+    input_n, *input_values = list(map(int, stdin.read().split()))
+    assert input_n == len(input_values)
+    print(partition3(input_values))
+
+
+
+
+
+
