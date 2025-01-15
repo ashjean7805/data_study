319c33a13fb3ef0a7dbbd794ad5adec5f26288ba
ashjean <ashjean.dev@gmail.com>
2025-01-15 22:08:05 +0900

    2025-01-15 Java 스터디 파일 추가

--- Java/First/src/array/ArrayTest.java
+++ Java/First/src/array/ArrayTest.java
@@ -8,13 +8,20 @@ public class ArrayTest {
 //		int[] studentIDs = new int[3] {101,102,103}; // 에러 확인
 //		int[] studentIDs = {101,102,103};
 
-		int[] studentIDs;
-		studentIDs = new int[] {101,102,103};
-        System.out.println(studentIDs[0]);
-        
-        studentIDs[0] = 10;
-        System.out.println(studentIDs[0]);
-				
+//		int[] studentIDs;
+//		studentIDs = new int[] {101,102,103};
+//        System.out.println(studentIDs[0]);
+//        
+//        studentIDs[0] = 10;
+//        System.out.println(studentIDs[0]);
+
+		int[] num = new int[] {1,2,3,4,5,6,7,8,9,10};
+		for(int i = 0;i < num.length; i++) {
+			System.out.println(i);
+			System.out.println(num[i]);
+			System.out.println('/');
+		}
+		
 	}
 
 }
